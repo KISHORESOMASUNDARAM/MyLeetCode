8be551b6f4cfeada7fd56eca8d785bfc939b7b61
KISHORESOMASUNDARAM <143739282+KISHORESOMASUNDARAM@users.noreply.github.com>
2024-07-07 12:37:53 +0530

    Time: 35 ms (46.22%), Space: 62 MB (29.36%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,22 +1,30 @@
 class Solution {
     public long maximumHappinessSum(int[] hap, int k) {
-        long val=0;
-        int cnt=0,n1=0;
+        long val=0,n1=0;
+        int cnt=0;
         int max=Integer.MIN_VALUE;
         int n=hap.length;
-        long ans=0;
         Arrays.sort(hap);
         if(k==1){
-            return hap[n-1];
+            for(int i=0;i<n;i++){
+            if(hap[i]>max){
+                max=Math.max(hap[i],max);
+            }
+            }
+            return max;
         }
         for(int i=n-1;i>=0;i--){
-            if(hap[i]-cnt>=0 && n1<k){
-            val=Math.max(hap[i]-cnt,0);
-            ans+=val;
+            if(i==n-1){
+               val=hap[i];
+               cnt++;
+            }
+            else if(hap[i]-cnt>=0 && n1<k){
+                val+=Math.max(hap[i]-cnt,0);
+                cnt++;
             }
             n1++;
-            cnt++;
+
         }
-        return ans;
+        return val;
     }
 }
